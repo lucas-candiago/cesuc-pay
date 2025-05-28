96a7f760181734a63eecd28243e2d39dae511fbc
Lucas Candiago <lucas.candiago@gmail.com>
2025-05-28 14:41:38 -0300

    fix: return api to port 8000

--- backend/server.js
+++ backend/server.js
@@ -14,5 +14,5 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/users", require("./routes/authRoutes"))
 app.use("/transactions", require("./routes/transactionRoutes"))
 
-const PORT = process.env.PORT || 8000;
+const PORT = 8000
 app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
